7927f46431c6124c1e61dac1ca037da0dc97290f
F99 <farajiomar99@gmail.com>
2025-11-10 09:42:28 +0100

    refactor: simplify ErrorResponse and update ResourceNotFoundException constructor

--- src/main/java/com/example/tricol/tricolspringbootrestapi/exception/ResourceNotFoundException.java
+++ src/main/java/com/example/tricol/tricolspringbootrestapi/exception/ResourceNotFoundException.java
@@ -1,7 +1,8 @@
 package com.example.tricol.tricolspringbootrestapi.exception;
 
 public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String resourceName, Long id) {
-        super(String.format("%s not found with id: %d", resourceName, id));
+    public ResourceNotFoundException(String message) {
+        super(message);
     }
 }
+
